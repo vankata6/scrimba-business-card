2bc5c87f1f143801cdc3dcfc45a0112f10d1832e
vankata6 <ikkolev95@gmail.com>
2022-01-25 07:17:28 +0200

    adding styles to main content

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Main() {
     return(
-        <>
+        <div className="main">
             <h2 className="main-name">Ivan Kolev</h2>
             <h5 className="main-occupation">Web developer</h5>
             <h6 className="main-site">https://github.com/vankata6</h6>
@@ -20,7 +20,7 @@ function Main() {
                 fanatic. Bacon buff. Entrepreneur. Travel geek. Pop <br/> 
                 culture ninja. Coffee fanatic.</p>
             </section>
-        </>
+        </div>
     )
 }
 
