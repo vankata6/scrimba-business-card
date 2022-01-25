15679af2707f16413aa4fe55b66410c731bdc09a
vankata6 <ikkolev95@gmail.com>
2022-01-25 09:08:44 +0200

    styling the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Footer() {
     return(
-        <>
-            <a href="#" class="fa fa-facebook"></a>
+        <div className="footer">
             <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-facebook"></a>
             <a href="#" class="fa fa-instagram"></a>
             <a href="#" class="fa fa-linkedin"></a>
             <a href="#" class="fa fa-github"></a>
-        </>
+        </div>
     )
 }
 
